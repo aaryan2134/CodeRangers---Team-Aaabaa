ceb71ce0ba218c3f610a91e6799286d6e4c8ed1e
Aayush Patel <901ayush@gmail.com>
2024-01-19 12:53:45 +0530

    Added Website for demonstration

---
+++ Aaabaa_Travel-Booking-Website/src/components/Hotels.jsx
@@ -0,0 +1,27 @@
+import { Heading } from "@chakra-ui/react";
+import React from "react";
+import "../styles/Hotels.css";
+
+const Hotels = () => {
+  return (
+    <div id='hotels'>
+      <div id='text'>
+        <Heading>Great Locations across the World</Heading>
+        <div className='desc'>
+          <p>
+            Travel to the most breathtaking and exotic location across the globe
+            with us.
+          </p>
+        </div>
+      </div>
+      <div>
+        <img
+          src='https://inato.com/static/094c124358273399e0abb5a636383989/0d4fb/inato-world.webp'
+          alt='map'
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Hotels;
